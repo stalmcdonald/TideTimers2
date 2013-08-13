478caf83e58841f4b2a92f1fd87cadbc9fb74dcb
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-08-12 17:35:59 -0700

    Project Creation
    Set up for week 2

---
+++ src/com/cm/tidetimers2/MainActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Crystal McDonald
+ * Java II
+ * 1308
+ * Week 2
+ * Tidal Prediction Application
+ * 
+ */
+package com.cm.tidetimers2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
